00d44c1364ac37014a2f8254e4606b1364f40781
Delablette <35489458+Delablette@users.noreply.github.com>
2018-04-03 11:12:24 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -67,8 +67,8 @@ UINT8 conversion_character (char var1, char var2){
             hexadecimal = 0xFF &((var1 - '0')<<4);
 
     }
-    else if ('A'<= var1 && var1 <='F'){
-        hexadecimal = (var1 - 55)<<4;
+    else if ('a'<= var1 && var1 <='f'){
+        hexadecimal = (var1 - 61)<<4;
     }
     else{
          fprintf(stderr, "erreur de nombre saisi\n");
@@ -77,36 +77,27 @@ UINT8 conversion_character (char var1, char var2){
    if ('0'<=var2 && var2<='9'){
             hexadecimal += (var2 - '0');
    }
-    else if ('A'<= var2 && var2<='F'){
-        hexadecimal += (var2 - 55);
+    else if ('a'<= var2 && var2<='f'){
+        hexadecimal += (var2 - 61);
     }
     else{
          fprintf(stderr, "erreur de nombre saisi\n");
-         return 1;void calc_crc16_sur_la_chaine (UINT8 string3[], int len){
-reg = 6363;
-for(int i= 0; i< len; i++)
-{
-    printf("l'octet dans la fonction chaine: %p\n", string3[i]);
-    calc_crc16_sur_un_octet(string3[i]);
-}
-   calc_crc16_sur_un_octet ((UINT8) 0x0);
-   calc_crc16_sur_un_octet ((UINT8) 0x0);
-
-   printf("res: %p\n", get_reste(reg));
-}
+         return 1;
     }
+
     return hexadecimal;
 }
 
 void calc_crc16_sur_la_chaine (UINT8 string3[], int len){
-reg = 0xFFFF;
-for(int i= 0; i< len; i++)
+set_reste(reg, 0x6363);
+for(int i=0; i< len; i++)
 {
     printf("l'octet dans la fonction chaine: %p\n", string3[i]);
     calc_crc16_sur_un_octet(string3[i]);
 }
-   calc_crc16_sur_un_octet ((UINT8) 0x0);
-   calc_crc16_sur_un_octet ((UINT8) 0x0);
+   calc_crc16_sur_un_octet ((UINT8) 0x00);
+   calc_crc16_sur_un_octet ((UINT8) 0x00);
+
 
    printf("res: %p\n", get_reste(reg));
 }
@@ -115,16 +106,18 @@ void calc_crc16_sur_un_octet (UINT8 nouvel_octet){
 int bdpf;
 printf("octet dans la chaine: %p\n", nouvel_octet);
 set_octet(reg, nouvel_octet);
-for(int i=0; i<8;i++){
-reg = reg <<1;
+    for(int i=0; i<8;i++){
+        reg = reg <<1;
 
-if((get_excedant(reg) & 0x01) == 1){
-    bdpf = 1;
-}
-else
-    bdpf = 0;
-if (bdpf== 1){
-    set_reste(reg, (get_reste(reg)^ POLYNOME_CRC16));
-}
-}
+    if((get_excedant(reg) & 0x01) == 1){
+        bdpf = 1;
+        }
+    else
+        bdpf = 0;
+    if (bdpf== 1){
+        set_reste(reg, (get_reste(reg)^ POLYNOME_CRC16));
+        }
+    }
+
+    printf("\nresultat intermediaire: %p\n",get_reste(reg));
 }
